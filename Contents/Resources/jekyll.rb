2443cd3b66e1998f1f0efe35ebbf2c41085365b3
Roben Kleene <contact@robenkleene.com>
2020-04-24 17:07:16 -0400

    Fix wrapping Jekyll

--- Contents/Resources/jekyll.rb
+++ Contents/Resources/jekyll.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby --disable-gems
 
-require 'optparse'
-
 require_relative 'bundle/bundler/setup'
-# require 'repla'
 
-require_relative 'lib/runner'
+# require 'repla'
+# require_relative 'lib/runner'
+
+ENV["PATH"] = ENV["PATH"].split(":").push("/Users/robenkleene/Development/Projects/Cocoa/Repla/Packages/Jekyll.replaplugin/Contents/Resources/bin/").join(":")
+
+Dir.chdir('/Users/robenkleene/Development/Projects/Cocoa/Repla/Packages/Jekyll.replaplugin/Contents/Resources/test/data/repla-test-jekyll'){
+  # %x[/Users/robenkleene/Development/Projects/Cocoa/Repla/Packages/Jekyll.replaplugin/Contents/Resources/bin/bundle exec jekyll serve --watch]
+  Kernel.exec('/Users/robenkleene/Development/Projects/Cocoa/Repla/Packages/Jekyll.replaplugin/Contents/Resources/bin/bundle exec jekyll serve --watch')
+}
 
 # Repla.clean_path
-runner = Repla::Jekyll::Runner.new(command, config)
-trap 'SIGINT' do
-  runner.stop
-end
+# runner = Repla::Jekyll::Runner.new(command, config)
+# trap 'SIGINT' do
+#   runner.stop
+# end
 
-runner.run
+# runner.run
